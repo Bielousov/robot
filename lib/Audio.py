15a7b5f9a3cad64696b90f37be27ecda064c035d
Anton Bielousov <anton.bielousov@gmail.com>
2025-01-06 11:12:02 -0800

    revert stream callback

--- lib/Audio.py
+++ lib/Audio.py
@@ -1,10 +1,9 @@
 from time import sleep
-from pyaudio import PyAudio, paContinue, paInt8
+from pyaudio import PyAudio, paInt8
 
 class Audio:
     def __init__(self):
         self.audio = PyAudio()
-        self.buffer = []
         self.channels=1
         self.format=paInt8
         self.sample_rate=24_000
@@ -14,30 +13,7 @@ class Audio:
             channels=self.channels,
             rate=self.sample_rate,
             output=True,
-            stream_callback=self.audio_callback,
         )
-
-    def audio_callback(self, in_data, frame_count, time_info, status):
-        """Non-blocking callback for PyAudio stream."""
-        if self.buffer:
-            # Pop the first chunk from the queue
-            chunk = self.buffer.pop(0)
-        else:
-            # If no data available, send silence
-            chunk = b'\x00' * frame_count * self.channels
-
-        return (chunk, paContinue)
-
-
-    def output(self, chunk):
-        self.buffer.append(chunk)
         
-
-    def start(self):
-        self.stream.start_stream()
-
-    def stop(self):
-        self.stream.stop_stream()
-
-    def clear(self):
-        self.audio.stop()
+    def output(self, audio):
+        self.stream.write(audio)
--- lib/OpenAiClient.py
+++ lib/OpenAiClient.py
@@ -1,4 +1,3 @@
-from io import BytesIO
 from openai import OpenAI
 
 class OpenAiClient:

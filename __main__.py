e87392147b4aa1f58c28ad45d0e68fc045320be9
Anton Bielousov <anton.bielousov@gmail.com>
2025-01-07 15:31:09 -0800

    remove v2 references

--- __main__.py
+++ __main__.py
@@ -6,11 +6,11 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'lib'))
 # Set the path for the v3 directory
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'v3'))
 
-import v2.main
+import v3.main
 
 def exitSignal(signal, frame):
     print("Handling exit signal:", signal)
-    v2.main.shutdown()
+    v3.main.shutdown()
     sys.exit(0)
 
 if __name__ == "__main__":
@@ -18,4 +18,4 @@ if __name__ == "__main__":
     signal.signal(signal.SIGTERM, exitSignal)
 
     # Run the entry point of v2 main.py when the root-level main.py is executed
-    v2.main.start()  # Assuming v2/main.py has a `run()` function or the desired entry point
+    v3.main.start()  # Assuming v2/main.py has a `run()` function or the desired entry point

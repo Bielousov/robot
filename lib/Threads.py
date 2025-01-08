aee16f0dd9b0c2349491ea9652fe9244203156ac
Anton Bielousov <anton.bielousov@gmail.com>
2025-01-07 18:28:44 -0800

    update Process start/stop

--- lib/Threads.py
+++ lib/Threads.py
@@ -29,26 +29,22 @@ class Process():
     self.args=args
     self.target = target
 
-    self.event = threading.Event()
+    self.runningEvent = threading.Event()
     self.thread = threading.Thread(target=self._process)
     # Daemonize the thread to exit when the main program ends
     self.thread.daemon = True 
+    self.thread.start()
 
     self.start()
     
   def _process(self):
     while True:
-      self.event.wait()  # Will pause if the event is not set
+      self.runningEvent.wait()  # Will pause if the event is not set
       self.target(self.args)
-
-  def pause(self):
-    self.event.clear()
 
   def start(self):
     if not self.thread.is_alive():
-      self.thread.start()
-    self.event.set()
+      self.runningEvent.set()
   
   def stop(self):
-    if self.thread.is_alive():
-      self.thread.stop()
+      self.runningEvent.clear()

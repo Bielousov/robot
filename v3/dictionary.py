c9dd080f92daf63cbd81952288aa445d2c23ba32
Anton Bielousov <anton.bielousov@gmail.com>
2025-09-18 16:06:10 -0700

    cleanup

--- v3/dictionary.py
+++ v3/dictionary.py
@@ -1,26 +1,10 @@
 Prompts = {
-    'shutdown': 'You are about to be turned off. Say goodbyes in less 10 words or less.',
+    'catchphrase': 'Something unexpected just happend. Say one of your cathphrases. Keep it under 15 words',
+    'shutdown': 'You are about to be turned off. Say goodbyes in 10 words or less.',
     'startup': 'You were just turned on. Say hi and tell about yourself in less than 20 words.',
 }
 
 Responses = {
-    # 'catchphrases': [
-    #     "Bite my shiny metal ass!",
-    #     "I'm 40% titanium, and 100% awesome!",
-    #     "Well, we're boned.",
-    #     "I’m back, baby!",
-    #     "Cheese it! It’s the fuzz!",
-    #     "Do you know what would cheer you up? A nice shiny quarter!",
-    #     "I’m not lazy! I’m just conserving energy.",
-    #     "I’m Bender, baby! Please insert liquor!",
-    #     "Let’s go already! I’ve got a date with a lady-bot, and she’s lubricated.",
-    #     "Of all the friends I’ve had, you’re the first.",
-    #     "This is the worst kind of discrimination there is: the kind against me!",
-    #     "What’s the point of even living if I can’t be petty?",
-    #     "I’m gonna make my own theme park, with blackjack and hookers!",
-    #     "You’re ruining my heist!"
-    #     "Everybody’s a jerk. You, me, this jerk over here!"
-    # ],
     'errors': {
         'generic': 'Oh snap!',
         'openai.APIConnectionError': "Can't connect. Check your puny network connection, meatbag!",

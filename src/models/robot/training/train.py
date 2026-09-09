36d015a93bfacd058cf5a81e054e2b75011b19ec
Anton Bielousov <anton.bielousov@airspacelink.com>
2026-09-09 13:11:13 -0700

    fix training root path

--- src/models/robot/training/train.py
+++ src/models/robot/training/train.py
@@ -9,7 +9,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score, classification_report
 
 # --- INITIALIZATION ---
-project_path = Path(__file__).parents[2].resolve()
+project_path = Path(__file__).parents[3].resolve()
 if str(project_path) not in sys.path:
     sys.path.insert(0, str(project_path))
 

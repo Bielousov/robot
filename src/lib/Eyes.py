ff4d7c83b8352ecc87cb4ba9c54ab8d173395697
Anton Bielousov <anton.bielousov@gmail.com>
2026-09-19 16:20:38 -0700

    fix 2 eyes seen

--- src/lib/Eyes.py
+++ src/lib/Eyes.py
@@ -24,7 +24,7 @@ class Eyes:
         self.height = height
         self.cascaded = cascaded
         self.rotate = 1
-        self.orientation = 0
+        self.orientation = 1
         self.contrast = 64
 
         self.focusPoint = [0, 0]  # [x, y]
